6b5fc82bd2ddc14b09bd7154a2be1b48d84195b3
nasuhafaith <102500099+nasuhafaith@users.noreply.github.com>
2022-03-28 12:28:59 +0800

    Add files via upload

---
+++ assignment1.sql
@@ -0,0 +1,10 @@
+ create database db1;
+ create table db1.student (
+ id int not null,
+ name varchar(45),
+ department varchar(45),
+ primary key(id)
+ );
+ insert into student values (0001, 'Mimi', 'SE');
+ insert into student values (0002, 'Momo', 'SE');
+
